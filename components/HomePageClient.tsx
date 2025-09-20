f57c4f8f9f1fbade3ef168d323559e6d5bba0925
Mounika <mounika.m@xitsolutionsinc.com>
2025-09-19 23:30:40 -0400

    title

--- components/HomePageClient.tsx
+++ components/HomePageClient.tsx
@@ -67,7 +67,7 @@ const HomePageClient: React.FC = () => {
         </div>
 
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold tracking-tight text-gray-900 dark:text-white leading-tight">
-            The Ultimate Frontend Developer Toolbox
+            The Ultimate "One-Stop" Toolbox
         </h1>
         
         <div className="mt-8">
