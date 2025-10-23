a08b5e07dc105c84185f7e88cb70e9b5b8cee89e
Mounika <mouni.murugonda@gmail.com>
2025-10-23 18:34:33 -0400

    scroll isssue

--- app/tips/layout.tsx
+++ app/tips/layout.tsx
@@ -1,5 +1,6 @@
 import TipsSidebar from '@/components/TipsSidebar';
 import Footer from '@/components/Footer';
+import BuyMeACoffeeCard from '@/components/BuyMeACoffeeCard';
 
 export default function TipsLayout({
   children,
@@ -12,6 +13,10 @@ export default function TipsLayout({
       <main className="w-full md:flex-1 bg-gray-50 dark:bg-gray-900 flex flex-col h-[calc(100vh-81px)] overflow-y-auto">
         <div className="flex-grow p-4 md:p-8">
           {children}
+          {/* Buy me a coffee section as scrollable component */}
+          <div className="mt-8 p-4 md:p-8 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 rounded-lg">
+            <BuyMeACoffeeCard />
+          </div>
         </div>
         <Footer />
       </main>
--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -1,5 +1,6 @@
 
 import Sidebar from "@/components/Sidebar";
+import BuyMeACoffeeCard from "@/components/BuyMeACoffeeCard";
 
 export default function ToolsLayout({
   children,
@@ -12,6 +13,10 @@ export default function ToolsLayout({
       <main className="w-full md:flex-1 bg-gray-50 dark:bg-gray-900 flex flex-col h-[calc(100vh-81px)] overflow-y-auto">
         <div className="flex-grow p-4 md:p-6">
           {children}
+          {/* Buy me a coffee section as scrollable component */}
+          <div className="mt-8 p-4 md:p-6 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 rounded-lg">
+            <BuyMeACoffeeCard />
+          </div>
         </div>
       </main>
     </div>
